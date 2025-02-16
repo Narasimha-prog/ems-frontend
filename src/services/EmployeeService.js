898ca60e1ca800a0075a4ef7d33716d7ceaba4e5
Dileep <panabakadileep@gmail.com>
2025-02-16 14:58:58 +0530

    Initial commit

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const REST_API_BASE_URL="http://localhost:8080/api/employee"
+const REST_API_BASE_URL="http://ec2-13-60-16-159.eu-north-1.compute.amazonaws.com:8080/api/employee"
 export const listEmployees =()=> axios.get(REST_API_BASE_URL);
 
 export const createEmployee=(employee)=> axios.post(REST_API_BASE_URL,employee)
